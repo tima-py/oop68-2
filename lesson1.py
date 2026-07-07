f90000a37650bc00efac93030366368b16c74cf2
tima-py <tykvaplayground@gmail.com>
2026-07-07 18:46:49 +0600

    second commit

--- lesson1.py
+++ lesson1.py
@@ -2,16 +2,40 @@
 # hero_mage
 class Hero:
     # Конструктор класса
-    def __init__(self, name, hp, lvl):
+    def __init__(self, name, hp, lvl, strength):
         # Атрибуты экземпляра класса
         self.name = name
         self.hp = hp
         self.lvl = lvl
+        self.strength = strength
+    def greet(self):
+        return f'Привет, я {self.name}, мой уровень {self.lvl}'
+    def attack(self):
+        self.strength -= 1
+        return f'{self.name} наносит удар!'
+    def rest(self):
+        self.hp += 1
+        return f'{self.name} отдыхает...'
 
 
 # Объект | Экземпляр класса
-kirito = Hero('Kirito', 1000, 100)
-asuna = Hero('Asuna', 1000, 100)
+kirito = Hero('Kirito', 1000, 100, 50)
+asuna = Hero('Asuna', 1000, 100, 40)
 
+print('-------------------')
 print(kirito.name)
+print(kirito.greet())
+print(kirito.attack())
+print(kirito.strength)
+print(kirito.rest())
+print(kirito.hp)
+print('-------------------')
+
+print('-------------------')
 print(asuna.name)
+print(asuna.greet())
+print(asuna.attack())
+print(asuna.strength)
+print(asuna.rest())
+print(asuna.hp)
+print('-------------------')
